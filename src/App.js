8fb020070f9c90bbaa81100414a7153f569a9edc
Muneeb926595 <31837560+Muneeb926595@users.noreply.github.com>
2020-06-12 18:42:15 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react';
+import './App.css';
+import { findByLabelText } from '@testing-library/react';
+
+function App() {
+  let [count,setCount]=useState(29)
+  let [isDay,setDay]=useState(true)
+  return (
+    <div className={`box ${isDay?'dayLight':''}`}>
+      <div className="content">
+      <h1>Welcome To My New Page</h1>
+      <button onClick={()=> setDay(!isDay)}>Day/Light</button>
+      <br/ >
+  <h1>Hi There Your Current Room Temprature is : {count}</h1>
+
+  <span><h2 className="myText">Set Temprature</h2></span>
+      <button onClick={()=> setCount(--count)}>-</button>
+      <button onClick={()=> setCount(++count)}>+</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
